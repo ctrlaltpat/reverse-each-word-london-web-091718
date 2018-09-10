46f199e85cb02f3137576283f85bc52218b957e9
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-10 20:18:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  words = sentence.split(" ")
-  words_reversed = []
-  words.each do |word|
-    words_reversed << word.reverse
-  end
-  words_reversed.join(" ")
+  # words = sentence.split(" ")
+  # words_reversed = []
+  # words.each do |word|
+  #   words_reversed << word.reverse
+  # end
+  # words_reversed.join(" ")
 end
